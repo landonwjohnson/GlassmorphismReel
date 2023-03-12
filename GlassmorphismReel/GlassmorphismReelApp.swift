d49a2649851b5f679f9ef8f06ecf4c91a1e019cf
“Landon <landonwjohnson@gmail.com>
2023-03-12 15:10:44 -0600

    Initial Commit

---
+++ GlassmorphismReel/GlassmorphismReelApp.swift
@@ -0,0 +1,17 @@
+//
+//  GlassmorphismReelApp.swift
+//  GlassmorphismReel
+//
+//  Created by Landon Johnson on 3/12/23.
+//
+
+import SwiftUI
+
+@main
+struct GlassmorphismReelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
